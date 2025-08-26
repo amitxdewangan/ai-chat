564ef0155d46fae45e6a27bd68e6f0f6c3f97610
Amit-TheOne <amit.dew009@gmail.com>
2025-08-26 20:29:16 +0530

    update db name

--- lib/db.ts
+++ lib/db.ts
@@ -6,11 +6,15 @@ export const connectDB = async () => {
   if (isConnected) return;
   try {
     await mongoose.connect(process.env.MONGODB_URI as string, {
-      dbName: "chatgpt_clone",
+      dbName: "ai_chat",
     });
     isConnected = true;
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
   }
+  // finally {
+  //   // Ensures that the client will close when you finish/error
+  //   await mongoose.disconnect();
+  // }
 };

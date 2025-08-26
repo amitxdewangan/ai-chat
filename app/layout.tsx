17b497b100d02350d7f92f16de4d765680d6bf30
Amit-TheOne <amit.dew009@gmail.com>
2025-08-26 18:08:48 +0530

    add Sidebar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/components/sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen w-screen flex bg-zinc-950 text-zinc-100`}
       >
-        {children}
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Main Chat */}
+        <main className="flex-1 flex flex-col">
+          {children}
+        </main>
       </body>
     </html>
   );

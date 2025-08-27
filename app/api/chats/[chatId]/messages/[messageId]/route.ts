13918f47f11db58b61eb2bf51173b9313dd170bf
Amit-TheOne <amit.dew009@gmail.com>
2025-08-27 20:39:33 +0530

    add message/[messageId] API

---
+++ app/api/chats/[chatId]/messages/[messageId]/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { connectDB } from "@/lib/db";
+import Message from "@/models/message";
+
+export async function PATCH(req: Request, { params }: { params: { chatId: string; messageId: string } }) {
+  try {
+    await connectDB();
+    const data = await req.json();
+    const updated = await Message.findOneAndUpdate(
+      { _id: params.messageId, chatId: params.chatId },
+      data,
+      { new: true }
+    );
+
+    if (!updated) {
+      return NextResponse.json({ error: "Message not updated" }, { status: 404 });
+    }
+
+    return NextResponse.json(updated);
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to update message", err: error }, { status: 500 });
+  }
+}
